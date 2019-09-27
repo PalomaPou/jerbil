3f035e3fd516cb9ac988020f0cf7bab6023aa4cc
DW-Stross <daniel@good-loop.com>
2019-09-27 16:42:54 +0100

    sync

--- src/java/com/goodloop/jerbil/JerbilConfig.java
+++ src/java/com/goodloop/jerbil/JerbilConfig.java
@@ -13,7 +13,7 @@ import com.winterwell.utils.time.TUnit;
 
 public class JerbilConfig {
 	
-	public static final String VERSION = "0.5.4";
+	public static final String VERSION = "0.5.5";
 	
 	@Override
 	public String toString() {
